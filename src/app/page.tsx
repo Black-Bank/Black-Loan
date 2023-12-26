c4ded0bd4632f294e2e179090aa1d786a51f353a
Guilherme Silva <2003silvagui@gmail.com>
2023-12-26 19:07:06 -0300

    feat: page login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-import Login from '@/screens/Login/Login';
-import Image from 'next/image';
+import FormLogin from "@/components/Login/Login";
+import Image from "next/image";
+import background from "../assets/background/background.svg";
 
-export default function Home() {
-	return (
-		<>
-			<Login />
-		</>
-	);
+export default function Login() {
+  return (
+    <>
+      <main className="w-screen h-screen flex justify-center items-center lg:bg-[#9377ef2b]">
+        <div className="w-full flex justify-center xs:w-96 sm:shadow py-10 rounded-2xl lg:hidden">
+          <FormLogin />
+        </div>
+        <div className="hidden lg:flex w-[900px] h-5/6 shadow-2xl rounded-2xl bg-white">
+          <div className="w-6/12 flex to-[#9377EF] from-[#6e4ed6] bg-gradient-to-r justify-center items-center rounded-2xl">
+            <FormLogin />
+          </div>
+          <div className="w-6/12 flex">
+            <Image src={background} alt="icon" className="-mt-10" />
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
