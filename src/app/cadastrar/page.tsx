0d82d7d5f85e1d9120e1f3c659df096ec81bf3c5
Guilherme Silva <2003silvagui@gmail.com>
2023-12-26 20:09:56 -0300

    feat: page register

--- src/app/cadastrar/page.tsx
+++ src/app/cadastrar/page.tsx
@@ -1,20 +1,20 @@
-import FormLogin from "@/components/Login/Login";
 import Image from "next/image";
-import background from "../assets/background/background.svg";
+import background from "../../assets/background/back.svg";
+import FormRegister from "@/components/Register/Register";
 
 export default function Login() {
   return (
     <>
       <main className="w-screen h-screen flex justify-center items-center lg:bg-[#9377ef2b]">
         <div className="w-full flex justify-center xs:w-96 sm:shadow py-10 rounded-2xl lg:hidden animate-fade-down animate-once">
-          <FormLogin />
+          <FormRegister />
         </div>
         <div className="hidden lg:flex w-[900px] h-5/6 shadow-2xl rounded-2xl bg-white animate-fade-down animate-once">
-          <div className="w-6/12 flex to-[#9377EF] from-[#6e4ed6] bg-gradient-to-r justify-center items-center rounded-2xl">
-            <FormLogin />
-          </div>
           <div className="w-6/12 flex">
             <Image src={background} alt="icon" className="-mt-10" />
+          </div>
+          <div className="w-6/12 flex from-[#9377EF] to-[#6e4ed6] bg-gradient-to-r justify-center items-center rounded-2xl">
+            <FormRegister />
           </div>
         </div>
       </main>
